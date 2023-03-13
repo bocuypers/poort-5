aad8f6d7de7fd28d5514e7d1d8b7aa73392c7df3
bocuypers <123274149+bocuypers@users.noreply.github.com>
2023-03-13 09:35:44 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let knuffelen = 0
+let voeden = 0
+let dood = 0
+basic.forever(function () {
+    knuffelen = 20
+    voeden = 20
+    if (false && (knuffelen > 10 && voeden > 10)) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        dood = 0
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    }
+})
